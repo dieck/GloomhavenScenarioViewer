72554d9a7e57c97502cf69c88bf3b7c16238050c
Marco Dieckhoff <marco.dieckhoff@googlemail.com>
2018-10-28 11:58:19 +0100

    added generation and copyright notice

---
+++ license.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+<title>Gloomhaven Scenario Viewer - License</title>
+
+<style>
+pre {
+    display: block;
+    font-family: monospace;
+    white-space: pre;
+    margin: 1em 0;
+}
+</style>
+
+</head>
+
+<body>
+
+<div class="link">
+<a href="index.php">Back to Gloomhaven Scenario Viewer</a>
+</div>
+
+<div class="license">
+<pre>
+<?php
+readfile("LICENSE");
+?>
+</pre>
+</div>
+
+</body>
+
+</html>
